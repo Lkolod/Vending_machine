10cdd9e04a13739c9852170ce113f3297fe1abfb
Łukasz Kołodziej <kolodziej051@gmail.com>
2023-05-17 01:12:30 +0200

    Update main.cpp

--- Vending_OOP/main.cpp
+++ Vending_OOP/main.cpp
@@ -2,8 +2,8 @@
 #include "VendingMachine.h"
 
 int main() {
-    int salacted_id;
-    int salacted_qty;
+    int SelectedID;
+    int Selected_QTY;
     char choice;
     double total_input_cash = 0;
     double COINS[6] = {5,2,1,0.5,0.2,0.1};
@@ -17,23 +17,23 @@ int main() {
     do {
         machine.dispProduct();
         cout << "Select id" << endl;
-        cin >> salacted_id;
-        cout << "You selected id:\t" << salacted_id << endl;
-        machine.Check_id(salacted_id);
+        cin >> SelectedID;
+        cout << "You selected id:\t" << SelectedID << endl;
+        machine.Check_id(SelectedID);
 
         cout << "Select quantity" << endl;
-        cin >> salacted_qty;
-        cout << "You selected quantity:\t" << salacted_qty << endl;
-        machine.checkQty(salacted_id,salacted_qty);
+        cin >> Selected_QTY;
+        cout << "You selected quantity:\t" << Selected_QTY << endl;
+        machine.checkQty(SelectedID, Selected_QTY);
 
-        if (salacted_qty > 0) {
-            machine.check_if_enough(salacted_id, salacted_qty, &total_input_cash);
+        if (Selected_QTY > 0) {
+            machine.check_if_enough(SelectedID, Selected_QTY, &total_input_cash);
         }
         machine.dispProduct();
         cout << "\n\n-->Do you want to continue (y/n)? ";
         cin >> choice;
 
-    } while((choice == 'y' || choice== 'Y') && (salacted_id != 1223 || salacted_qty != 1223));
+    } while((choice == 'y' || choice== 'Y') && (SelectedID != 1223 || Selected_QTY != 1223));
 
     machine.Display_bought();
 
